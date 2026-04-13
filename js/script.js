a21680a9e8eaeff602629be1eb76e2b815a7e24f
ihodges5340 <ihodges5340@sdsu.edu>
2026-04-12 23:31:28 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-// ALL YOU JS GOES HERE
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('open');
+}
+
+function toggleTheme() {
+    const theme = document.getElementById("lightMode");
+    const current = theme.getAttribute("href");
+    const button = document.querySelector(".light-mode-button");
+    const logos = document.querySelectorAll(".nav-logo");
+
+    if (current === "css/styles.css") {
+        theme.setAttribute("href", "css/darkmode.css");
+        button.classList.add("toggled");
+        logos.forEach(logo => logo.setAttribute("src", "images/DFTI-Website-logo-dark.png"));
+        document.body.classList.add("dark-mode");
+    } else {
+        theme.setAttribute("href", "css/styles.css");
+        button.classList.remove("toggled");
+        logos.forEach(logo => logo.setAttribute("src", "images/DFTI-Website-logo.png"));
+        document.body.classList.remove("dark-mode");
+    }
+}
